adaf4fbab247ca073bb07c0171bd81cee231af37
dagongren7 <13023294697@163.com>
2021-11-21 23:15:50 +0800

    yaml完成

--- advanced/yamlDmeo/capability_yaml.py
+++ advanced/yamlDmeo/capability_yaml.py
@@ -1,15 +1,14 @@
 from appium import webdriver
 import yaml
 
-file=open('desired_caps.yaml','r')
-data=yaml.load(file)
+with open('desired_caps.yaml','r',encoding='utf-8') as f:
+    data = yaml.load(f,Loader=yaml.FullLoader)
 
 desired_caps={}
 desired_caps['platformName']=data['platformName']
 desired_caps['platformVersion']=data['platformVersion']
 desired_caps['deviceName']=data['deviceName']
 
-desired_caps['app']=data['app']
 desired_caps['appPackage']=data['appPackage']
 desired_caps['appActivity']=data['appActivity']
 desired_caps['noReset']=data['noReset']
--- advanced/yamlDmeo/yaml_dump.py
+++ advanced/yamlDmeo/yaml_dump.py
@@ -1,5 +1,5 @@
 import yaml
-
+# dump()可以将Python对象序列化成YAML流。如果stream为None，则返回生成的字符串
 slogan=['welcome','to','51zxw']
 website={'url':'www.51zxw.net'}
 
