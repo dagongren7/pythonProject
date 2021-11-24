fcbe70776191324ef701a55857c3a6971ed4da4d
dagongren7 <13023294697@163.com>
2021-11-24 22:33:05 +0800

    初版appium自动化+日志

---
+++ appiumDemo/advanced/page_object/loginView.py
@@ -0,0 +1,31 @@
+import logging
+from selenium.webdriver.common.by import By
+
+from appiumDemo.advanced.page_object.common_fun import Common
+from appiumDemo.advanced.page_object.desired_caps import appium_desired
+
+
+class LoginView(Common):
+    username_type=(By.ID,'com.tal.kaoyan:id/login_email_edittext')
+    password_type=(By.ID,'com.tal.kaoyan:id/login_password_edittext')
+    loginBtn=(By.ID,'com.tal.kaoyan:id/login_login_btn')
+
+    def login_action(self,username,password):
+        self.check_cancelBtn()
+        self.check_skipBtn()
+
+        logging.info('============login_action==============')
+        logging.info('username is:%s' %username)
+        self.driver.find_element(*self.username_type).send_keys(username)
+
+        logging.info('password is:%s'%password)
+        self.driver.find_element(*self.password_type).send_keys(password)
+
+        logging.info('click loginBtn')
+        self.driver.find_element(*self.loginBtn).click()
+        logging.info('login finished!')
+
+if __name__ == '__main__':
+    driver=appium_desired()
+    l=LoginView(driver)
+    l.login_action('自学网2018','zxw2018')
