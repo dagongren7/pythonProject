88774f37788bec611b3f70c113c48384c5c8b160
dagongren7 <13023294697@163.com>
2021-11-29 16:57:24 +0800

    Excel工具导入数据

--- seleniumDemo/two/ExcelUtil.py
+++ seleniumDemo/two/ExcelUtil.py
@@ -9,17 +9,18 @@ class ParseExcel():
     def getDatasFromSheet(self):
         dataList = []
         for line in self.sheet:
-            print(line)
             tmpList=[]
             tmpList.append(line[1].value)
             tmpList.append(line[2].value)
             dataList.append(tmpList)
+            #0 1 从第二行开始打印
+        print(dataList,"---------",dataList[1:])
         return dataList[1:]
 
 if __name__ == '__main__':
     excelPath='D:/test.xlsx'
     sheetName = '搜索数据表'
     pe = ParseExcel(excelPath)
-    for i in pe.getDatasFromSheet()[1:]:
-        print(i[0])
+    for i in pe.getDatasFromSheet()[0:]:
+        print("data: ",i[0])
     pass
